cd960943ad35dc679b837b2deec13b32149af1b9
Kota iwadate <beeguru122@gmail.com>
2016-05-21 19:46:40 +0900

    create user db, apply bootstrap

---
+++ test/controllers/user_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class UserControllerTest < ActionController::TestCase
+  test "should get name:string" do
+    get :name:string
+    assert_response :success
+  end
+
+  test "should get password:string" do
+    get :password:string
+    assert_response :success
+  end
+
+end
